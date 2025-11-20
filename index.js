bcd3204b4b65e9338bd154b4eea3d7a896e49c2e
Thelli Govinda Rao <147299435+govindthelli@users.noreply.github.com>
2025-11-20 15:33:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const http = require("http");
 
 http.createServer((req, res) => {
-    res.write("Deployment successful! Hello from index.js");
+    res.write(<div style="font-family: Arial, sans-serif; padding: 40px; text-align: center;">
+            <h1 style="color: #1e90ff; font-size: 42px;">🚀 Deployment Successful!</h1>
+            <p style="color: #28a745; font-size: 24px; font-weight: bold;">
+                Hello from <span style="color:#ff4500;">index.js from agent Server</span>
+            </p>
+        </div>);
     res.end();
 }).listen(4000);
